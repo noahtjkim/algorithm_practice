7f4387355a876a79dd7737e5a106a35d800940a8
Noah Kim <noah24.kim@gmail.com>
2023-03-19 16:48:33 -0500

    SortedIntegerbyTheNumberof1Bits commit

---
+++ SortIntegersbyTheNumberof1Bits.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def sortByBits(self, arr: list) -> list:
+
+        res = sorted(arr, key=lambda x: (str(bin(x)).count("1"), x))
+
+        return res
+
+sol = Solution()
+res = sol.sortByBits([1024,512,256,128,64,32,16,8,4,2,1])
+print(res)
+
+
+
+
+
+
+
