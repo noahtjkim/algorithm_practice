415d341adffae1017e2d75f4c6d1a6890726523a
Noah Kim <noah24.kim@gmail.com>
2023-03-23 00:54:49 -0500

    commit IsSubsequence

---
+++ IsSubsequence.py
@@ -0,0 +1,28 @@
+
+class Solution:
+    def isSubsequence(self, s: list, t: list) -> bool:
+        s = list(s)
+        t = list(t)
+        res = []
+        seq = 0
+        e_flag = False
+
+        if len(s) > 0:
+            for i in range(len(t)):
+                if t[i] == s[seq]:
+                    res.append(t[i])
+                    seq += 1
+                    if seq >= len(s):
+                        break
+
+        if res == s:
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
