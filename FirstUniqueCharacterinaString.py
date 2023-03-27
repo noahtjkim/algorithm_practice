e2b794531fb7f208b083283577745b3c21e8a7c5
Noah Kim <noah24.kim@gmail.com>
2023-03-27 11:53:38 -0500

    added faster code

--- FirstUniqueCharacterinaString.py
+++ FirstUniqueCharacterinaString.py
@@ -1,14 +1,28 @@
 
+from collections import Counter
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        # Faster code
+        cnt_dict = Counter(s)
         idx = -1
 
         for i in range(len(s)):
-            if s[i] not in s[i + 1:] + s[:i]:
+            if cnt_dict[s[i]] == 1:
                 idx = i
                 break
 
         return idx
+
+        # Slower code
+        # idx = -1
+        #
+        # for i in range(len(s)):
+        #     if s[i] not in s[i + 1:] + s[:i]:
+        #         idx = i
+        #         break
+        #
+        # return idx
 
 
 
