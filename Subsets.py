3fb3a7ce0c449a267927cdaae8e29d02b0b177a7
Noah Kim <noah24.kim@gmail.com>
2023-04-01 21:26:09 -0500

    commit 04/01/2023

---
+++ Subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtracking(n, subset, level):
+            if len(subset) == level:
+                if subset not in res:
+                    res.append(list(subset))
+
+            if len(subset) == len(nums):
+                return
+
+            if not n:
+                if subset not in res:
+                    res.append(list(subset))
+                return
+
+            for i in range(len(n)):
+                subset.append(n[i])
+                backtracking(n[i + 1:], subset, level + 1)
+                del subset[-1]
+
+        backtracking(nums, [], 0)
+
+        return res
