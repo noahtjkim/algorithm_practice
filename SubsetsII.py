18d55fc93653b16ec29ee98a411b74399c10e549
Noah Kim <noah24.kim@gmail.com>
2023-04-02 00:29:43 -0500

    commit 04/02/023

---
+++ SubsetsII.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def subsetsWithDup(self, nums: list) -> list:
+        res = []
+        nums = sorted(nums)
+
+        def backtracking(n, subs, depth):
+            if len(subs) == len(nums):
+                if subs not in res:
+                    res.append(list(subs))
+                return
+
+            if len(subs) == depth:
+                if subs not in res:
+                    res.append(list(subs))
+
+            for i in range(len(n)):
+                subs.append(n[i])
+                backtracking(n[i + 1:], subs, depth + 1)
+                del subs[-1]
+
+        res.append([])
+        backtracking(nums, [], 0)
+
+        return res
